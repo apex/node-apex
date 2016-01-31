f969d84742cb9c07019ef34f07b9eb5bf75bca3f
TJ Holowaychuk <tj@vision-media.ca>
2016-01-31 11:50:00 -0800

    add support for returning non-promises

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 module.exports = function λ(fn) {
   return function(e, ctx) {
     try {
-      fn(e, ctx)
-        .then(ctx.succeed)
-        .catch(ctx.fail)
+      var v = fn(e, ctx)
+
+      if (v && typeof v.then == 'function') {
+        v.then(ctx.succeed).catch(ctx.fail)
+        return
+      }
+
+      ctx.succeed(v)
     } catch (err) {
       ctx.fail(err)
     }
